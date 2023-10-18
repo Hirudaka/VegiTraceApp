024fa57ca775e64ee6c333687e2653e491e32892
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-10-19 03:31:29 +0530

    CentresOverview

---
+++ app/src/main/java/com/example/vegitrace/DambullaCentre.kt
@@ -0,0 +1,11 @@
+package com.example.vegitrace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DambullaCentre : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dambulla_centre)
+    }
+}
