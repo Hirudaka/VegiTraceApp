d8486674889d39ccd655047b12f70184fb376db0
hirudaka <hirudakakumarasingha@gmail.com>
2023-10-24 12:16:18 +0530

    added flow recycler and shop owner

--- app/src/main/java/com/example/vegitrace/WastageForm.kt
+++ app/src/main/java/com/example/vegitrace/WastageForm.kt
@@ -3,6 +3,7 @@ package com.example.vegitrace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.vegitrace.databinding.ActivityWastageFormBinding
 import com.google.firebase.database.DatabaseReference
@@ -55,6 +56,25 @@ class WastageForm : AppCompatActivity() {
         binding.wastageGoBack.setOnClickListener {
             finish()
         }
+
+
+        val historyButton = findViewById<ImageView>(R.id.navBookingUnClick)
+        val wastageMainButton = findViewById<ImageView>(R.id.navHomeUnClick)
+        val recyclerProfileButton = findViewById<ImageView>(R.id.navProfileUnClick)
+
+        wastageMainButton.setOnClickListener {
+            val intent = Intent(this, WastageMain::class.java)
+            startActivity(intent)
+        }
+        historyButton.setOnClickListener {
+            val intent = Intent(this, WastageHistory::class.java)
+            startActivity(intent)
+        }
+        recyclerProfileButton.setOnClickListener {
+            val intent = Intent(this, RecyclerProfile::class.java)
+            startActivity(intent)
+        }
+
     }
     data class WasteForm(
         val wastageName: String?=null,
